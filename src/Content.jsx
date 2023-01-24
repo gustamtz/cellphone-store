a981fc3a76ff0a51cff068bcc84fe955ce8b3b19
Gustavo Medeiros <eugus03@gmail.com>
2023-01-24 13:51:07 -0300

    fix: reacte router import

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route } from "react-route-dom";
-import {} from "./pages/Store";
+import { Switch, Route } from "react-router-dom";
+import { Store } from "./pages/Store";
 
 export const Content = () => {
   return (
