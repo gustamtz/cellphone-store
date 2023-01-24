584026b4a9cb7f3dc74f2565eac84f0963114470
Gustavo Medeiros <eugus03@gmail.com>
2023-01-24 16:53:46 -0300

    feat: hooks added

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BsFillCartPlusFill, BsFillCartCheckFill } from "react-icons/bs";
+import { getItem, setItem } from "../services/LocalStorageFuncs";
 
 export const Store = () => {
-  return <div>Store</div>;
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = "https://api.mercadolibre.com/sites/MLB/search?q=celular";
+      const response = await fetch(url);
+      const objJson = await response.json();
+      setData(objJson.results);
+    };
+    fetchApi();
+  }, []);
+
+  const handleClick = (obj) => {
+    const element = cart.find((e) => e.id === obj.id);
+    if (element) {
+      const arrFilter = cart.filter((e) => e.id !== obj.id);
+      setCart(arrFilter);
+    } else {
+      setCart([...cart, obj]);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Store</h1>
+      <div>
+        {data.map((e) => (
+          <div key={e.id}>
+            <h4>{e.title}</h4>
+            <img src={e.thumbnail} alt="" />
+            <h4>{e.price}</h4>
+            <button onClick={() => handleClick(e)}>
+              {cart.some((itemCart) => itemCart.id === e.id) ? (
+                <BsFillCartCheckFill />
+              ) : (
+                <BsFillCartPlusFill />
+              )}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
