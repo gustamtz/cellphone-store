188e99d62243190fca6fa1851f8c5a9cf6643558
Gustavo <eugus03@gmail.com>
2023-01-25 13:41:15 -0300

    fix: import cart component

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { Cart } from "./pages/Cart";
 import { Store } from "./pages/Store";
 
 export const Content = () => {
   return (
     <Switch>
+      <Route exact path="/cart" component={Cart} />
       <Route exact path="/" component={Store} />
     </Switch>
   );
