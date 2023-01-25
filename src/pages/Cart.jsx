82ef3990ab8b2f0e3dc62b52f12a0eddfc7de93f
Gustavo <eugus03@gmail.com>
2023-01-25 17:52:21 -0300

    feat: create remove item function

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
-import { getItem } from "../services/LocalStorageFuncs";
+import { getItem, setItem } from "../services/LocalStorageFuncs";
+import { BsCartDashFill } from "react-icons/bs";
 
 export const Cart = () => {
-  const [data, setData] = useState(getItem("cart" || []));
+  const [data, setData] = useState(getItem("objcart" || []));
+
+  const removeItem = (obj) => {
+    const arrFilter = data.filter((e) => e.id !== obj.id);
+    setData(arrFilter);
+    setItem("objcart", arrFilter);
+  };
 
   return (
     <div>
-      <h1>Carrinho</h1>
+      <h1>Cart</h1>
+      <div>
+        {data.map((e) => (
+          <div key={e.id}>
+            <h4>{e.title}</h4>
+            <img src={e.thumbnail} alt={e.title} />
+            <h4>{e.price}</h4>
+            <button onClick={() => removeItem(e)}>
+              <BsCartDashFill />
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
