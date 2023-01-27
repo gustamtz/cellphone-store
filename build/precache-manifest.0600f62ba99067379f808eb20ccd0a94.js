bcf92207616f2cc8144b099a8e29fc881f895681
Gustavo <eugus03@gmail.com>
2023-01-27 17:35:00 -0300

    create build

---
+++ build/precache-manifest.0600f62ba99067379f808eb20ccd0a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "587dcf10556add150878",
+    "url": "/static/css/main.37b98268.chunk.css"
+  },
+  {
+    "revision": "587dcf10556add150878",
+    "url": "/static/js/main.587dcf10.chunk.js"
+  },
+  {
+    "revision": "1c4b492de01783c5acc0",
+    "url": "/static/js/1.1c4b492d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49719751b26d6c5944ddec5d6a9ccf04",
+    "url": "/index.html"
+  }
+];
