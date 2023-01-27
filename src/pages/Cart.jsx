9b4116b1765c2682b1e7fd91ee5993c3b61cf96f
Gustavo <eugus03@gmail.com>
2023-01-27 12:39:29 -0300

    feat: implementing reduce

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { getItem, setItem } from "../services/LocalStorageFuncs";
 import { BsCartDashFill } from "react-icons/bs";
+import { ProductsArea } from "../css/pages";
+import { Header } from "../components/Header";
 
 export const Cart = () => {
   const [data, setData] = useState(getItem("objcart" || []));
@@ -11,21 +13,24 @@ export const Cart = () => {
     setItem("objcart", arrFilter);
   };
 
+  const subTotal = data.reduce((acc, cur) => acc + cur.price, 0);
+
   return (
     <div>
-      <h1>Cart</h1>
-      <div>
+      <Header />
+      <h3>{`Total: R$ ${subTotal}`}</h3>
+      <ProductsArea>
         {data.map((e) => (
           <div key={e.id}>
             <h4>{e.title}</h4>
             <img src={e.thumbnail} alt={e.title} />
-            <h4>{e.price}</h4>
+            <h4>{`R$ ${e.price}`}</h4>
             <button onClick={() => removeItem(e)}>
               <BsCartDashFill />
             </button>
           </div>
         ))}
-      </div>
+      </ProductsArea>
     </div>
   );
 };
